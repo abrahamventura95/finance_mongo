38d906fb6cbd5253013a380db89fc9e0da1294b4
Abraham Ventura <amsventura.95@gmail.com>
2020-08-05 11:48:19 -0400

    Coins + money
    Coins CRUD, Money update, delete and fix bug that use id for email

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var auth = require('../Middleware/auth');
 var UserController = require('../Controllers/user');
-/* GET users listing. */
+
 router.post('/login', UserController.login);
 router.post('/register', UserController.register);
 router.get('/all', UserController.getAll);
